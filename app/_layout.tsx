4b52f27515f8023ccb80cef9508682743babe107
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-06-23 19:04:11 +0500

    Added auth configuration from appwrite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,31 @@
-import { Stack } from "expo-router";
-import {Text} from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native"; 
 
+function RouteGuard({children} : { children: React.ReactNode }){
+
+    const [isSignUp, setisSignUp] = useState<boolean>(false)
+    const isAuth = false
+    const router = useRouter()
+
+    useEffect(() => {
+      if(!isAuth) {
+        router.replace("/auth");
+      }
+      else {
+        setisSignUp(true)
+      }
+    });
+
+    return <>{children}</>
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown:false }}/>
-    </Stack>
+            // <RouteGuard>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown:false }}/>
+              </Stack>
+            // </RouteGuard>  
   );
 }
